607915e07d585cb96b2b80c1ce5b183fc5254cd6
Jose Cuervo <jocuervo@bancolombia.com.co>
2024-02-03 11:31:47 -0500

    feat: Update Jest configuration and package dependencies

---
+++ src/components/ProductDisplay.spec.js
@@ -0,0 +1,10 @@
+import { mount } from "@vue/test-utils";
+import ProductDisplay from "./ProductDisplay.vue";
+
+describe("ProductDisplay", () => {
+  test("renders the component", () => {
+    const wrapper = mount(ProductDisplay);
+    // check that the wrapper contains the component
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
